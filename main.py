b6bcffb49b2e5f37b3715b6f642ada17f108c4c5
ibrah <ibrahima.toybou@coursdiderot.com>
2025-11-05 16:49:09 +0100

    commit q4

--- main.py
+++ main.py
@@ -13,3 +13,5 @@ print(f"La boutique {nom_boutique} vend des {produit} au prix de {prix_unitaire}
 prix_ht = round(prix_unitaire, 2)
 prix_ttc = round(prix_ht * (1 + tva), 2)
 print(f"Le prix hors taxe est de {prix_ht}€ et le prix toutes taxes comprises est de {prix_ttc}€.")
+
+nb_paires = int(input("Combien de paires de chaussures souhaitez-vous acheter ? "))
