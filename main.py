0eca2291f16ce501b0893c8c0de1131706754b82
ibrah <ibrahima.toybou@coursdiderot.com>
2025-11-05 16:19:33 +0100

    commit q1

---
+++ main.py
@@ -0,0 +1,7 @@
+nom_boutique  = str("ChossettZ")
+produit : str = "Chaussures"
+prix_unitaire = float (5.99)
+quantite_stock : int = 20
+tva : float = 0.20
+compte_client  = float(100)
+compte_boutique : float = 0
